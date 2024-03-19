b3f2955d5b98f6d94cd2d105e7c41b6fc1b8d7e3
Anastasia <71143459+AnastasiaMorozovaBPI205@users.noreply.github.com>
2024-03-19 17:42:36 +0300

    add user tests

--- src/test/java/app/bluefig/UserTest.java
+++ src/test/java/app/bluefig/UserTest.java
@@ -14,9 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -58,9 +58,10 @@ public class UserTest {
                     "password": "1234"
                 }""";
 
-        Mockito.when(this.service.findUserJpaByUsernamePasswordHash("Bobbo", "1234")).
-                thenReturn(getUserJpa());
-        Mockito.when(this.mapper.UserJpaToUser(getUserJpa())).thenReturn(getUser());
+        UserJpa user = getUserJpa();
+        Mockito.when(this.service.findUserJpaByUsernamePasswordHash("Bobbo", "1509442")).
+                thenReturn(user);
+        Mockito.when(this.mapper.UserJpaToUser(user)).thenReturn(getUser());
 
         mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON)
                 .content(bodyAuthorization))
@@ -69,23 +70,80 @@ public class UserTest {
     }
 
     @Test
-    void deleteUserByIdTest() {
+    void getUserByIdTest() throws Exception {
+        UserJpa user = getUserJpa();
+        Mockito.when(this.service.findUserJpaById("1")).thenReturn(user);
+        Mockito.when(this.mapper.UserJpaToUser(user)).thenReturn(getUser());
+
+        mockMvc.perform(get("/user/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"));
 
     }
 
     @Test
-    void getDoctorsPatientsTest() {
-
+    void deleteUserByIdTest() throws Exception {
+        mockMvc.perform(delete("/user/1")).andExpect(status().isOk());
     }
 
     @Test
-    void getPatientsDoctorsTest() {
+    void getDoctorsPatientsTest() throws Exception {
+        List<UserJpa> users = getUserJpas();
+        Mockito.when(this.service.findPatientsDoctorsJpa("1")).thenReturn(users);
+        Mockito.when(this.mapper.UserJpasToUsers(users)).thenReturn(getUsers());
 
+        mockMvc.perform(get("/doctors_list/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("2"));
     }
 
     @Test
-    void linkPatientToDoctorTest() {
+    void getPatientsDoctorsTest() throws Exception {
+        UserJpa userJpa = getUserJpa();
+        List<UserJpa> users = List.of(userJpa);
+        Mockito.when(this.service.findDoctorsPatientsJpa("2")).thenReturn(users);
+        Mockito.when(this.mapper.UserJpasToUsers(users)).thenReturn(List.of(getUser()));
 
+        mockMvc.perform(get("/patients_list/2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("1"));
+    }
+
+    @Test
+    void linkPatientToDoctorTest() throws Exception {
+        mockMvc.perform(post("/link_patient/1/2")).andExpect(status().isOk());
+    }
+
+    private List<User> getUsers() {
+        User user = new User();
+        user.setUsername("Fobbo");
+        user.setId("2");
+        user.setBirthday(LocalDate.parse("2024-03-13"));
+        user.setEmail("fobbo@gmail.com");
+        user.setFirstname("Fob");
+        user.setFathername("-");
+        user.setLastname("Ross");
+        user.setPasswordHash("4321");
+        user.setRoleId("47b436f0-b4bb-11ee-8c0c-00f5f80cf8ae");
+        user.setSex("male");
+
+        return List.of(user);
+    }
+
+    private List<UserJpa> getUserJpas() {
+        UserJpa user = new UserJpa();
+        user.setUsername("Fobbo");
+        user.setId("2");
+        user.setBirthday(LocalDate.parse("2024-03-13"));
+        user.setEmail("fobbo@gmail.com");
+        user.setFirstname("Fob");
+        user.setFathername("-");
+        user.setLastname("Ross");
+        user.setPasswordHash("4321");
+        user.setRoleId("47b436f0-b4bb-11ee-8c0c-00f5f80cf8ae");
+        user.setSex("male");
+
+        return List.of(user);
     }
 
     private User getUser() {
@@ -99,6 +157,7 @@ public class UserTest {
         user.setLastname("Ross");
         user.setPasswordHash("4321");
         user.setRoleId("49e6f33b-b4bb-11ee-8c0c-00f5f80cf8ae");
+        user.setSex("male");
 
         return user;
     }
@@ -114,6 +173,7 @@ public class UserTest {
         user.setLastname("Ross");
         user.setPasswordHash("4321");
         user.setRoleId("49e6f33b-b4bb-11ee-8c0c-00f5f80cf8ae");
+        user.setSex("male");
 
         return user;
     }

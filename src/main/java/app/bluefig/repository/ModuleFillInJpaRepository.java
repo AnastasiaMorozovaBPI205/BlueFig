efdb0c6e8f15517b97a279e4235d1b409162010d
Anastasia <71143459+AnastasiaMorozovaBPI205@users.noreply.github.com>
2024-04-18 17:43:13 +0300

    sort module fill ins

--- src/main/java/app/bluefig/repository/ModuleFillInJpaRepository.java
+++ src/main/java/app/bluefig/repository/ModuleFillInJpaRepository.java
@@ -18,14 +18,16 @@ public interface ModuleFillInJpaRepository extends JpaRepository<ModuleFillInJpa
 
     @Query(value = "select questionary_fillin.id, questionary_fillin.questionary_id, questionary_fillin.datetime" +
             " from questionary_fillin join questionary on questionary.id = questionary_fillin.questionary_id" +
-            " where questionary.doctor_id = :doctor_id and questionary.patient_id = :patient_id",
+            " where questionary.doctor_id = :doctor_id and questionary.patient_id = :patient_id" +
+            "order by questionary_fillin.datetime desc",
             nativeQuery = true)
     List<ModuleFillInJpa> findModulesFillInJpaByPatientDoctorIds(@Param("doctor_id") String doctorId,
                                                                  @Param("patient_id") String patientId);
 
     @Query(value = "select questionary_fillin.id, questionary_fillin.questionary_id, questionary_fillin.datetime" +
             " from questionary_fillin join questionary on questionary.id = questionary_fillin.questionary_id" +
-            " where questionary.patient_id = :patient_id",
+            " where questionary.patient_id = :patient_id" +
+            "order by questionary_fillin.datetime desc",
             nativeQuery = true)
     List<ModuleFillInJpa> findModulesFillInJpaByPatientId(@Param("patient_id") String patientId);
 
@@ -33,7 +35,8 @@ public interface ModuleFillInJpaRepository extends JpaRepository<ModuleFillInJpa
             select questionary_fillin.id, questionary_fillin.questionary_id, questionary_fillin.datetime\s
             from questionary_fillin\s
             join questionary on questionary.id = questionary_fillin.questionary_id\s
-            where questionary.patient_id = :patient_id and questionary.module_id = :module_id""",
+            where questionary.patient_id = :patient_id and questionary.module_id = :module_id
+            order by questionary_fillin.datetime desc""",
             nativeQuery = true)
     List<ModuleFillInJpa> findModulesFillInJpaByPatientIdModuleId(@Param("module_id") String moduleId,
                                                                  @Param("patient_id") String patientId);
@@ -42,7 +45,7 @@ public interface ModuleFillInJpaRepository extends JpaRepository<ModuleFillInJpa
             select questionary_fillin.id, questionary_fillin.questionary_id, questionary_fillin.datetime\s
             from questionary_fillin join questionary on questionary.id = questionary_fillin.questionary_id\s
             where questionary.patient_id = :patient_id and questionary_fillin.questionary_id = :questionary_id\s
-            order by datetime desc""",
+            order by questionary_fillin.datetime desc""",
             nativeQuery = true)
     List<ModuleFillInJpa> findModulesFillInJpaByPatientIdQuestionaryId(@Param("questionary_id") String questionaryId,
                                                                   @Param("patient_id") String patientId);
